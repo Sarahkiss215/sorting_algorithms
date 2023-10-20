d709684887cdf7ba74a8d1681052296aa609bb90
Sarahkiss215 <skisiangani5@gmail.com>
2023-10-20 00:49:02 -0700

    Add function to sort an array of integers in ascending order

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order 
+ * @array: pointer to array
+ * @size: size of the array
+ *
+ * Return: Nothing.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j, n;
+
+	for (;i < size; i++)
+	{
+		for (j = 1; j < size - i; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				n = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = n;
+				print_array(array, size);
+			}
+		}
+	}
+}
